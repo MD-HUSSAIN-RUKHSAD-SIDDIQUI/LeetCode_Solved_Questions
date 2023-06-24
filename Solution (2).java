62feafd374cc48062426ddddb23bae7a61210ec7
MD-HUSSAIN-RUKHSAD-SIDDIQUI <87136761+MD-HUSSAIN-RUKHSAD-SIDDIQUI@users.noreply.github.com>
2023-06-24 23:50:52 +0530

    Update Solution (4).java

--- Solution (2).java
+++ Solution (2).java
@@ -1,22 +1,15 @@
 class Solution {
-    public int searchInsert(int[] nums, int target) {
-        int low=0;
-        int high=nums.length-1;
-        while(low<=high)
-        {
-            int mid=low+(high-low)/2;
-            if(nums[mid]==target)
-            {
-                return mid;
-
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+         int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
             }
-            else if(nums[mid]>target)
-            {
-                high=mid-1;
-            }
-            else
-            low=mid+1;
         }
-        return low;
     }
 }
