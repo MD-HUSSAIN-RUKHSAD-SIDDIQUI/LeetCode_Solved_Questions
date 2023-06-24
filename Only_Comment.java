ff284646fafafef9c80929a6a9d5d1183c935431
MD-HUSSAIN-RUKHSAD-SIDDIQUI <87136761+MD-HUSSAIN-RUKHSAD-SIDDIQUI@users.noreply.github.com>
2023-06-25 00:18:00 +0530

    Move_Zeroes Leetcode Problem

--- Only_Comment.java
+++
@@ -1,3 +0,0 @@
-//This is a main branch for  LeetCode_Solved_Questions.
-//Please Check Branches for Solved Questions.
-//Thanks.
---
+++ Solution(6).java
@@ -0,0 +1,23 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int size=nums.length;
+        if(size==0 || size==1)
+        return;
+      int z=0;
+      int nz=0;
+      while(nz<size)
+      if(nums[nz]!=0)
+      {
+          //swap(nums[nz],nums[z]);
+          int temp=nums[nz];
+          nums[nz]=nums[z];
+          nums[z]=temp;
+          z++;
+          nz++;
+      }  
+      else
+      {
+          nz++;
+      }
+    }
+}
